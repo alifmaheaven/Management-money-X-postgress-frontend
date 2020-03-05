51524bcafa8f79138a32c42bf917f6e67e2f5cca
Alif Maheaven <alifmaheaven2001@gmail.com>
2020-03-05 12:42:29 +0700

    adding bootstrap and etc library

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueSweetalert2);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
